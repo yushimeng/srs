62d0a6281f17381f6f6e83249b9542cf8c8152d0
yushimeng <shimeng.yu@qq.com>
2022-09-01 11:12:07 +0000

    improv: fix Dockerfile

--- trunk/src/app/srs_app_bandwidth_dector.cpp
+++ trunk/src/app/srs_app_bandwidth_dector.cpp
@@ -39,9 +39,6 @@ using namespace std;
 #define SRS_BANDWIDTH_DECTOR_PT_REQ 1
 #define SRS_BANDWIDTH_DECTOR_PT_RES 2
 #ifdef SRS_BANDWIDTH_DECTOR
-// SrsBandwidthDectorResponsePacket::SrsBandwidthDectorResponsePacket() {
-    
-// }
 
 SrsBandwidthDectorResponsePacket::~SrsBandwidthDectorResponsePacket() {
 
@@ -108,47 +105,6 @@ void SrsBandwidthDectorOverUdp::set_stfd(srs_netfd_t fd)
 {
     lfd = fd;
 }
-#if 0
-srs_error_t SrsBandwidthDectorOverUdp::on_udp_packet(const sockaddr* from, const int fromlen, char* buf, int nb_buf)
-{
-    srs_error_t err = srs_success;
-    char address_string[64];
-    char port_string[16];
-
-    // TODO： Hexadecimal print first n bytes
-
-    if(getnameinfo(from, fromlen, 
-                   (char*)&address_string, sizeof(address_string),
-                   (char*)&port_string, sizeof(port_string),
-                   NI_NUMERICHOST|NI_NUMERICSERV)) {
-        return srs_error_new(ERROR_SYSTEM_IP_INVALID, "bad address");
-    }
-    std::string peer_ip = std::string(address_string);
-    int peer_port = atoi(port_string);
-    if (nb_buf < SRS_BANDWIDTH_DECTOR_MIN_PACKET_SIZE) {
-        srs_warn("udp: wait %s:%d packet %d bytes", peer_ip.c_str(), peer_port, nb_buf);
-        return err;
-    }
-
-    SrsBuffer* stream = new SrsBuffer(buf, nb_buf);
-    SrsAutoFree(SrsBuffer, stream);
-    if ((err = pkt.decode(stream)) != srs_success) {
-        return srs_error_wrap(err, "pkt decode err");
-    }
-    
-    SrsBandwidthDectorResponsePacket response_pkt;
-    response_pkt.encode(&pkt);
-    std::string str = response_pkt.to_string();
-    
-    srs_trace("recv pkt: %s, response:%s", pkt.format_print().c_str(), response_pkt.format_print().c_str());
-
-    int ret = srs_sendto(lfd, (char*)str.c_str(), (int)str.length(), from, fromlen, SRS_UTIME_NO_TIMEOUT);
-    if (ret <= 0) {
-        srs_trace("bandwidth dector: send_message falid (%d)", ret);
-    }
-    return err;
-}
-#endif
 
 SrsBandwidthDectorRequestPacket::SrsBandwidthDectorRequestPacket() {
 
@@ -187,34 +143,6 @@ srs_error_t SrsBandwidthDectorRequestPacket::decode(SrsBuffer* stream) {
     
     return err;
 }
-#if 0
-std::string SrsBandwidthDectorRequestPacket::format_print() {
-    stringstream ss;
-    ss << "{"
-        << "v=" << std::to_string((int)version) << ","
-        << "pt=" << std::to_string((int)payload_type) << ","
-        << "length=" << std::to_string((int)length) << ","
-        << "seq=" << std::to_string(sequence_number) << ","
-        << "timestamp=" << std::to_string(timestamp)
-        << "}";
-    return ss.str();
-}
-#endif
-
-// SrsBandwidthDectorOverUdp::SrsBandwidthDectorOverUdp(SrsConfDirective* c)
-// {
-//     pprint = SrsPithyPrint::create_caster();
-// }
-
-// SrsBandwidthDectorOverUdp::~SrsBandwidthDectorOverUdp()
-// {
-//     srs_freep(pprint);
-// }
-
-// void SrsBandwidthDectorOverUdp::set_stfd(srs_netfd_t fd) 
-// {
-//     lfd = fd;
-// }
 
 srs_error_t SrsBandwidthDectorOverUdp::on_udp_packet(const sockaddr* from, const int fromlen, char* buf, int nb_buf)
 {
@@ -258,37 +186,7 @@ srs_error_t SrsBandwidthDectorOverUdp::on_udp_packet(const sockaddr* from, const
     sender->wakeup();
     return err;
 }
-#if 0
-// refs: https://rongcloud.yuque.com/iyw4vm/tgzi1r/epbdl3#zn6hj
-srs_error_t SrsBandwidthDectorRequestPacket::decode(SrsBuffer* stream) {
-    srs_error_t err = srs_success;
-    
-    // at least 16bytes header
-    if (!stream->require(16)) {
-        return srs_error_new(ERROR_RTP_HEADER_CORRUPT, "requires 16 only %d bytes", stream->left());
-    }
-    
-    int8_t vv = stream->read_1bytes();
-    version = (vv >> 6) & 0x03;
-    payload_type = vv & 0x3f;
-    if (version != 0 || payload_type != SRS_BANDWIDTH_DECTOR_PT_REQ) {
-        return srs_error_new(ERROR_BANDWIDTH_DECTOR_RTP_INVALID, 
-            "rtp v:%d pt:%d invalid", version, payload_type);
-    }
 
-    length = stream->read_3bytes();
-    if ((uint32_t)stream->left() < length) {
-        return srs_error_new(ERROR_BANDWIDTH_DECTOR_RTP_INVALID, 
-            "requires payload length %d only %d bytes", length, stream->left());
-    }
-
-    sequence_number = stream->read_4bytes();
-
-    timestamp = stream->read_8bytes();
-    
-    return err;
-}
-#endif
 std::string SrsBandwidthDectorRequestPacket::format_print() {
     stringstream ss;
     ss << "{"
@@ -304,49 +202,6 @@ std::string SrsBandwidthDectorRequestPacket::format_print() {
 SrsBandwidthDectorResponsePacket::SrsBandwidthDectorResponsePacket() {
     
 }
-
-// SrsBandwidthDectorResponsePacket::~SrsBandwidthDectorResponsePacket() {
-
-// }
-
-// std::string SrsBandwidthDectorResponsePacket::format_print() {
-//     stringstream ss;
-//     ss << "{"
-//         << "v=" << std::to_string(version) << "," 
-//         << "pt=" << std::to_string(payload_type) << "," 
-//         << "length=" << std::to_string(length) << "," 
-//         << "seq=" << std::to_string(sequence_number) << "," 
-//         << "send data length=" << std::to_string(send_data_length) << "," 
-//         << "send_timestamp=" << std::to_string(send_timestamp) << "," 
-//         << "recv_timestamp=" << std::to_string(recv_timestamp) 
-//         << "}";
-//     return ss.str();
-// }
-
-// void SrsBandwidthDectorResponsePacket::encode(SrsBandwidthDectorRequestPacket* pkt) {
-//     version = 0;
-//     payload_type = SRS_BANDWIDTH_DECTOR_PT_RES;
-//     length = 24; // fixed. seq(4Byte) + send data length(4Byte) + send timestamp(8Byte) + recv timestamp(8Byte)
-//     sequence_number = pkt->sequence_number;
-//     send_data_length = pkt->length;
-//     send_timestamp = pkt->timestamp;
-//     recv_timestamp = (srs_get_system_time() + 500 ) / 1000;
-// }
-
-// std::string SrsBandwidthDectorResponsePacket::to_string() {
-//     char buf[1500];
-//     // char buf[kRtpPacketSize];
-//     SrsBuffer stream(buf, sizeof(buf));
-
-//     stream.write_1bytes((version << 6) | payload_type); // v pt
-//     stream.write_3bytes(length); // length
-//     stream.write_4bytes(sequence_number); // sequence_number
-//     stream.write_4bytes(send_data_length); // send_data_length
-//     stream.write_8bytes(send_timestamp); // send_timestamp
-//     stream.write_8bytes(recv_timestamp); // recv_timestamp
-//     return std::string(stream.data(), stream.pos());
-// }
-
 
 srs_error_t SrsBandWidthDectorSender::do_cycle() 
 {
@@ -462,7 +317,6 @@ void SrsBandWidthDectorManger::add_sender_to_free_list(SrsBandWidthDectorSender*
 void SrsBandWidthDectorManger::remove_sender(SrsBandWidthDectorSender* sender)
 {
     free_sender_cnt--;
-    // busy_sender_cnt--;
     senders.erase(sender);
     manager->remove(sender);
 }
@@ -489,7 +343,6 @@ void SrsBandWidthDectorSender::waiting(srs_utime_t timeout){
 
 SrsBandWidthDectorSender::SrsBandWidthDectorSender(SrsGb28181Config *config)
 {
-    // trd = new SrsSTCoroutine("bandwidth_dector_sender", this);
     wait = srs_cond_new();
     lock = srs_mutex_new();
     timeout = config->timeout;
@@ -525,12 +378,10 @@ SrsBandWidthDectorSender::~SrsBandWidthDectorSender()
 
 void SrsBandWidthDectorSender::wakeup()
 {
-// #ifdef SRS_PERF_QUEUE_COND_WAIT
     if (mw_waiting) {
         srs_cond_signal(wait);
         mw_waiting = false;
     }
-// #endif
 }
 
 void SrsBandWidthDectorSender::init(srs_netfd_t lfd_, const sockaddr* from_, int fromlen_) {
